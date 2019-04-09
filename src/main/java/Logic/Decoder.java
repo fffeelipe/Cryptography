723503a6d3f8f94bf391ba8f6d9bc6c187abea70
Felipe Epia <lfepiar@unal.edu.co>
2019-04-08 20:28:48 -0500

    a que no me ves :V

---
+++ src/main/java/Logic/Decoder.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author fffeelipe
+ */
+public class Decoder {
+
+    public static void main(String[] args) {
+        BufferedReader objReader = null;
+        
+        String number = "";
+        
+        try {
+
+            objReader = new BufferedReader(new FileReader("material/number1.txt"));
+            
+            number = objReader.readLine();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+                if (objReader != null) {
+                    objReader.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        
+        //System.out.println(number.substring(0, 10));
+        
+        String[] chunks = number.split("1", 0);
+        TreeMap<String,Integer> occurrences = new TreeMap<>();
+        for(String s : chunks){
+            if(occurrences.containsKey(s)){
+                occurrences.put(s, occurrences.get(s)+1);
+            }else{
+                occurrences.put(s, 1);
+            }
+        }
+        
+
+    }
+}
